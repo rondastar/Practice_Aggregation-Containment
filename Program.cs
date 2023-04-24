ed438893c4a2f4cb15380866f2828f9655ed0daa
rondastar <116147793+rondastar@users.noreply.github.com>
2023-04-24 11:00:46 -0700

    Created prog120 instance of School Program and displayed prog120 information

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@
     {
         static void Main(string[] args)
         {
-            
+            // Create an instance of Instructor
+            Instructor willCram = new Instructor("Will", "Cram");
+            // Create an instance of school program (prog120)
+            SchoolProgram prog120 = new SchoolProgram("Programming", "120", "J102", willCram);
+            // Add students to the prog120 instance of the SchoolProgram roster
+            prog120.AddStudent("Kelly", "Mark", 98, 96);
+            prog120.AddStudent("Ronda", "Rutherford", 96, 98);
+            prog120.AddStudent("Cleo", "Cavalier", 99, 99);
+            // Display prog120 class information
+            Console.WriteLine(prog120.ToString());
+            Console.WriteLine($"Instructor: {prog120.Instructor.ToString()}");
+            // Display prog120 class roster
+            prog120.DisplayRoster();
         } // Main
     } // class Program
 } // namespace
--- SchoolProgram.cs
+++ SchoolProgram.cs
@@ -8,5 +8,47 @@ namespace Practice_Aggregation_Containment
 {
     internal class SchoolProgram
     {
+        string _className;
+        string _classNumber;
+        string _roomNumber;
+        Instructor _instructor; // Aggregation
+        List<Student> _classRoster; // Containment
+
+        public SchoolProgram(string className, string classNumber, string roomNumber, Instructor instructor)
+        {
+            _className = className;
+            _classNumber = classNumber;
+            _roomNumber = roomNumber;
+            _instructor = instructor;
+            _classRoster = new List<Student>();
+        } // constructor
+
+        public string ClassName { get => _className; set => _className = value; }
+        public string ClassNumber { get => _classNumber; set => _classNumber = value; }
+        public string RoomNumber { get => _roomNumber; set => _roomNumber = value; }
+        internal Instructor Instructor { get => _instructor; set => _instructor = value; }
+        internal List<Student> ClassRoster { get => _classRoster; }
+
+        public void AddStudent(Student student)
+        {
+            ClassRoster.Add(student);
+        }
+        public void AddStudent(string firstName, string lastName, int csiGrade, int genEdGrade)
+        {
+            Student student = new Student(firstName, lastName, csiGrade, genEdGrade);
+            ClassRoster.Add(student);
+        }
+
+        public void DisplayRoster()
+        {
+            for (int i = 0; i < ClassRoster.Count; i++)
+            {
+                Console.WriteLine($"{ClassRoster[i].FirstName} {ClassRoster[i].LastName} - CSI Grade {ClassRoster[i].CsiGrade} - Gen Ed Grade {ClassRoster[i].GenEdGrade}");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{ClassName} {ClassNumber} - Room {RoomNumber} - Instructor: {Instructor} - Number of Students: {ClassRoster.Count}";
+        }
     } // class
 } // namespace
