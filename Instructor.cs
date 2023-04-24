20ab2f122225fb5638d401cfdfc2ccdf94784477
rondastar <116147793+rondastar@users.noreply.github.com>
2023-04-24 10:16:17 -0700

    In Instructor Class, added fields, constructor, and override to string

--- Instructor.cs
+++ Instructor.cs
@@ -8,5 +8,21 @@ namespace Practice_Aggregation_Containment
 {
     internal class Instructor
     {
+        string _firstName;
+        string _lastName;
+
+        public Instructor(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
     } // class
 } // namespace
